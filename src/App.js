a6b2fba7366ed1702a97fa6588e9cd298ce3041d
gozde <gozdeoztoprak0@gmail.com>
2021-02-22 11:33:10 +0100

    New head added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to my world...
         </a>
       </header>
     </div>
